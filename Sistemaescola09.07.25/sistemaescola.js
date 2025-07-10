0a6e7f4108767284046d9ba226b81e7548e52338
Fabiana <0001162789@senaimgaluno.com.br>
2025-07-10 16:12:11 -0300

    SistemaEscola

--- Sistemaescola09.07.25/sistemaescola.js
+++ Sistemaescola09.07.25/sistemaescola.js
@@ -1,41 +1,95 @@
-let prompt = require("prompt-sync")();
+let prompt = require('prompt-sync')();
+
 let alunos = [];
+let cursos = [];
+
 let opcao;
-do{
-    opcao = prompt("1 - Cadastrar aluno\n" + 
-                    "2 - Excluir aluno\n" + 
-                    "3 - Relatório aluno\n" +
-                    "4 - Sair sitema\n\n" +
-                    "Escolha uma opção\n");
 
- switch(opcao){
-    case "1":
-    let nomealuno = prompt("Digite o nome do aluno:");
-    if (nomealuno !== null && nomealuno.trim() !== ''){
-    alunos.push(nomealuno.trim);
-console.log(alunos)
-    }Fa
-    break;
+do {
+    opcao = prompt(
+                  'Sistema Escolar\n\n'+
+                  '1 - Cadastrar aluno\n' + 
+                  '2 - Excluir aluno\n' +
+                  '3 - Cadastrar curso\n' +
+                  '4 - Excluir curso\n' +
+                  '5 - Visualizar Informações\n'+
+                  '6 - Sair\n\n'+
+                  'Escolha uma opção:\n ' );
 
-    case "2":
+    switch(opcao){
+        case '1':
+            let nomeAluno = prompt('Digite o nome do aluno: ');
+            if (nomeAluno !== null && nomeAluno.trim() !== '') {
+                alunos.push(nomeAluno.trim);
+                console.log("Aluno cadastrado com sucesso!");
+            } else {
+                console.log("Nome inválido.");
+            }
+            break; 
 
-    break;
-    case "3":
+        case '2':
+            let nomeExcluir = prompt('Digite o nome do aluno a excluir: ');
+            let index = alunos.indexOf(nomeExcluir);
+            if (index !== -1) {
+                alunos.splice(index, 1);
+                console.log("Aluno excluído com sucesso!");
+            } else {
+                console.log("Aluno não encontrado.");
+            }
+            break;
 
-        //visualizar informações
-        let info = "alunos cadastrados:\n";
-        if (alunos.lenght > 0) {
-        for (let i = 0; i < alunos.length; i++) {
-            info += "-" + alunos [i] + "\n";
-        }}
+        case '3':
+            //cadastrar curso 
+            let nomeCurso = prompt('Digite nome do curso:');
+            if (nomeCurso !== null && nomeCurso.trim() !== '') {
+                cursos.push(nomeCurso.trim()); 
+                console.log("Curso cadastrado com sucesso!");
+            } else {
+                console.log("Nome inválido.");
+            }
+            break;
 
-        }else{
+        case '4':
+            //Excluir curso
+            let nomeExcluircurso = prompt('Digite o nome do curso a excluir:');
+            let indexCurso = curso.indexOf(nomeExcluirCurso);
+            if (indexCurso !== -1) {
+                cursos.splice(indexCurso, 1);
+                console.log("Curso excluido com sucesso!");
+            } else {
+                console.log("Curso não encontrado.");
+            }
+            break;
+        
+        case '5':
+            //visualizar informações
+            let info = "\nAlunos Cadastrados:\n";
+            if (alunos.length > 0){ 
+                for (let i = 0; i < alunos.length; i++) { 
+                    info += "-" + alunos[i] + "\n";
+                    }
+                    } else {
+                        info += 'Nenhum aluno cadastrado.\n';
+                        }
+                       info += "\nCursos Cadastrados:\n";
+                       if (cursos.length > 0) {        
+                            for (let i = 0; i < cursos.length; i++){
+                                info += '- ' + cursos[i] + '\n';
+                            }
+                        } else {
+                            info +='Nenhum curso cadastrado.\n';
+                        }
+                        console.log(info);
+                        break;
+                        
+        case "6":
+            //Sair do sistema
+            console.log("Saindo do sistema...");
+            break;
 
-        info += 'Nenhum aluno cadastrado.\n';
-        }
-    
-    console.log(info);
-    break;
-
-    }while(opcao !=4);
+            default:
+                console.log("Opção inválida. Tente novamente.");
+                
+    }
+    } while (opcao !== "6");
     
